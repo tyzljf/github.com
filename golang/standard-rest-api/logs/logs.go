a30897ab137afc14d6de24f58b117b8394ebcdad
tyzljf <tanyouzhang@sina.com>
2018-02-08 18:11:26 +0800

    1. 具备基础功能的日志工具

--- golang/standard-rest-api/logs/logs.go
+++ golang/standard-rest-api/logs/logs.go
@@ -24,15 +24,19 @@ const (
 
 //implement logger instance singleton
 var tlLogger *Logger
+var tlSync   sync.Mutex
 
-func NewLogger() *Logger {
-	tlLogger = &Logger{
-		Level: INFO,
-		FileName: DEFAULT_LOG_NAME,
-		LogCount: 3,
-		LogMaxSize: DEFAULT_LOG_SIZE,
+func init() {
+	tlSync.Lock()
+	defer tlSync.Unlock()
+	if tlLogger == nil {
+		tlLogger = &Logger{
+			Level: INFO,
+			FileName: DEFAULT_LOG_NAME,
+			LogCount: DEFAULT_LOG_COUNT,
+			LogMaxSize: DEFAULT_LOG_SIZE,
+		}
 	}
-	return tlLogger
 }
 
 type Logger struct {
@@ -82,48 +86,83 @@ func (tl *Logger) setLogMaxSize(logMaxSize int) {
 }
 
 func (tl *Logger) writeMsg(level int, msg string, v ...interface{}) error {
-	if level > tl.Level {
+	if level < tl.Level {
 		return nil
 	}
 
 	if len(v) > 0 {
-		msg = fmt.Sprintf(msg, v)
+		msg = fmt.Sprintf(msg, v...)
 	}
 
 	//compose the log message
-	head := tl.makeHead(level)
+	head := tl.formatHead(level)
+	time_str := tl.formatTime()
+
+	msg = fmt.Sprintf("[%s][%s]%s\n", time_str, head, msg)
+	return tl.writeToLogger(msg)
 }
 
-func (tl *Logger) makeHead(level int) string {
-	head := ""
+func (tl *Logger) writeToLogger(msg string) error {
+	if tl.fileWriter != nil {
+		tl.fileWriter.Close()
+	}
+
+	file, err := os.OpenFile(tl.FileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		return err
+	}
+	tl.fileWriter = file
+
+	_, err =  tl.fileWriter.Write([]byte(msg))
+	return err
+}
+
+func (tl *Logger) formatTime() string {
+	return time.Now().Format("2006/01/02 15:04:05")
+}
+
+func (tl *Logger) formatHead(level int) string {
+	head := " "
 
 	switch level {
 	case TRACE:
-		head := "TRACE"
+		head = "TRACE"
+		break
+	case DEBUG:
+		head = "DEBUG"
 		break
 	case INFO:
-
+		head = "INFO"
+		break
+	case WARN:
+		head = "WARN"
+		break
+	case CRIT:
+		head = "CRIT"
+		break
 	}
+
+	return head
 }
 
-func Trace(msg string, v ...interface{}) {
-	tlLogger.writeMsg(TRACE, msg, v)
+func Trace(format string, v ...interface{}) {
+	tlLogger.writeMsg(TRACE, format, v...)
 }
 
-func Debug(msg string, v ...interface{}) {
-	tlLogger.writeMsg(DEBUG, msg, v)
+func Debug(format string, v ...interface{}) {
+	tlLogger.writeMsg(DEBUG, format, v...)
 }
 
-func Info(msg string, v ...interface{}) {
-	tlLogger.writeMsg(INFO, msg, v)
+func Info(format string, v ...interface{}) {
+	tlLogger.writeMsg(INFO, format, v...)
 }
 
-func Warn(msg string, v ...interface{}) {
-	tlLogger.writeMsg(WARN, msg, v)
+func Warn(format string, v ...interface{}) {
+	tlLogger.writeMsg(WARN, format, v...)
 }
 
-func Crit(msg string, v ...interface{}) {
-	tlLogger.writeMsg(CRIT, msg, v)
+func Crit(format string, v ...interface{}) {
+	tlLogger.writeMsg(CRIT, format, v...)
 }
 
 
